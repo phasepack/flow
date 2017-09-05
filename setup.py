417f395fd210fdba64ace0eae5dc49b916b6328d
Noah Singer <singerng@gmail.com>
2017-09-05 09:11:13 -0400

    renamed repo

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 install_requires = ["numpy", "scipy"]
 
-setup(name="flowarch",
+setup(name="flow",
       version="0.1",
       description="A framework for constructing numerical algorithms from linearly or iteratively chainable flows",
-      url="https://github.com/phasepack/flowarch",
+      url="https://github.com/phasepack/flow",
       author="Noah Singer",
       author_email="",
       license="GNU GPLv3",
-      packages=["flowarch"],
+      packages=["flow"],
       zip_safe=False)
