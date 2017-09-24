a3bad5731b04140eaa61d80a01e2e7d2d5090776
Noah Singer <singerng@gmail.com>
2017-09-24 18:13:30 -0400

    in a working state

--- flow/__init__.py
+++ flow/__init__.py
@@ -1,4 +1,5 @@
 from typing import Any, Callable, Dict, List
+from time import time as _time
 
 
 class Var:
@@ -7,8 +8,8 @@ class Var:
         self.name = name
         self.docs = docs
 
-    def __str__(self):
-        return self.name
+    def __repr__(self):
+        return "<flow.Var : {}>".format(self.name)
 
 
 class Tape:
@@ -43,10 +44,28 @@ class Tape:
 class State:
     def __init__(self):
         self.values = {}
+        self.loop_stack = []
         self.tapes = {}
+        self.saved_tapes = {}
 
-    def __getitem__(self, var):
-        return self.values[var]
+    def get_tape(self, key):
+        if isinstance(key, int):
+            key = self.loop_stack[key]
+        return self.tapes[key]
+
+    def __getitem__(self, key):
+        if isinstance(key, Var):
+            return self.values[key]
+        elif isinstance(key, tuple):
+            if len(key) == 3:
+                loop = key[0]
+                var = key[1]
+                index = key[2]
+            else:
+                loop = -1
+                var = key[0]
+                index = key[1]
+            return self.get_tape(loop)[var,index]
 
     def __setitem__(self, var, value):
         self.values[var] = value
@@ -55,10 +74,15 @@ class State:
         del self.values[var]
 
     def push_loop(self, loop):
+        self.loop_stack.append(loop)
         self.tapes[loop] = Tape(loop)
 
-    def pop_loop(self, loop):
+    def pop_loop(self):
+        loop = self.loop_stack.pop()
+        tape = self.tapes[loop]
         del self.tapes[loop]
+        if loop.save:
+            self.saved_tapes[tape.loop] = tape
 
     def advance(self, loop):
         self.tapes[loop].advance(self.values)
@@ -67,22 +91,25 @@ class State:
         s = "State[\n"
         for var in self.values:
             s += "    " + var.name + " => " + str(self.values[var]) + "\n"
-        for tape in self.tapes.values():
-            s += str(tape) + "\n"
+        for loop in self.loop_stack:
+            s += str(self.tapes[loop]) + "\n"
         return s + "]"
 
 
 class Flow:
-    def __init__(self, flow_op: Callable[[Dict, Dict, State], None]):
+    def __init__(self, flow_op: Callable[[Dict, State], None]):
         self.flow_op = flow_op
 
-    def operate(self, inputs, options, state):
-        self.flow_op(inputs, options, state)
+    def operate(self, inputs, state):
+        self.flow_op(inputs, state)
 
     def __rshift__(self, other: "Flow") -> "Flow":
-        def chain(inputs, options, state):
-            self.operate(inputs, options, state)
-            other.operate(inputs, options, state)
+        if other is None:
+            return self
+
+        def chain(inputs, state):
+            self.operate(inputs, state)
+            other.operate(inputs, state)
         return Flow(chain)
 
 
@@ -93,43 +120,77 @@ def test_condition(state):
 
 
 class Loop(Flow):
-    def __init__(self, body_flow: Flow, condition_flow: Flow, loop_vars: List[Var]):
+    def __init__(self, body_flow: Flow, condition_flow: Flow, loop_vars: List[Var], save: bool=False):
         self.body_flow = body_flow
         self.condition_flow = condition_flow
         self.vars = loop_vars
+        self.save = save
 
-        def flow_op(inputs, options, state):
+        def flow_op(inputs, state):
             state.push_loop(self)
-            self.condition_flow.operate(inputs, options, state)
+            state.advance(self)
+            self.condition_flow.operate(inputs, state)
 
             while test_condition(state):
+                self.body_flow.operate(inputs, state)
+                self.condition_flow.operate(inputs, state)
                 state.advance(self)
-                self.body_flow.operate(inputs, options, state)
-                self.condition_flow.operate(inputs, options, state)
 
-            state.pop_loop(self)
+            state.pop_loop()
 
         super(Loop, self).__init__(flow_op)
 
 
-def flow(flow_op: Callable[[Dict, Dict, State], None]) -> Flow:
+def flow(flow_op: Callable[[Dict, State], None]) -> Flow:
     return Flow(flow_op)
+
+# Useful flow decorators
+
+
+def inspect(f):
+    def inspected_flow(inp, state):
+        print(str(state))
+        f.operate(inp, state)
+    return Flow(inspected_flow)
+
+
+TIME = Var('t', """The system time.""")
+
+
+def time(f):
+    def timed_flow(inp, state):
+        f.operate(inp, state)
+        state[TIME] = _time()
+    return Flow(timed_flow)
 
 
 CONDITION = Var('?', """The condition supplied to switches and loops.""")
-LOOP_COUNTER = Var('#', """The number of iterations in a loop.""")
 
 
 def switch(yes: Flow, no: Flow=None) -> Flow:
-    def check(inputs, options, state):
+    def check(inputs, state):
         # Check the condition that was passed in
         condition = state[CONDITION]
         del state[CONDITION]
 
         if condition:
             if yes:
-                yes.operate(inputs, options, state)
+                yes.operate(inputs, state)
         else:
             if no:
-                no.operate(inputs, options, state)
+                no.operate(inputs, state)
     return Flow(check)
+
+
+#
+# # Backtracking
+# M = flow.Var('M', "The maximum value of f over the last several iterations.")
+#
+# @flow
+# def backtracking_condition(inp, state):
+#     state[flow.CONDITION] = state[f] - \
+#                             (state[M] + np.real(state[Dx].ravel().T @ state[gradf, -1].ravel()) + la.norm(state[Dx].ravel()) ** 2 / (2 * state[tau, -1])) > inp['EPSILON']
+#
+# @flow
+# def backtracking_window_estimator(inp, state):
+#     state[M] =
